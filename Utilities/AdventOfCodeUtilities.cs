61db4c30961d6c87fc993993b39feead1bedf239
Richard Carter <Richard-CRT@users.noreply.github.com>
2019-12-04 09:03:27 +0000

    Add project files.

---
+++ Utilities/AdventOfCodeUtilities.cs
@@ -0,0 +1,59 @@
+﻿//#define OVERRIDE
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeUtilities
+{
+    public class AoCUtilities
+    {
+        static public void DebugClear()
+        {
+#if DEBUG || OVERRIDE
+            Console.Clear();
+#endif
+        }
+
+        static public string DebugReadLine()
+        {
+#if DEBUG || OVERRIDE
+            return Console.ReadLine();
+#else
+            return "";
+#endif
+        }
+
+        static public void DebugWriteLine()
+        {
+#if DEBUG || OVERRIDE
+            Console.WriteLine();
+#endif
+        }
+
+        static public void DebugWriteLine(string text, params object[] args)
+        {
+#if DEBUG || OVERRIDE
+            string lineToWrite = string.Format(text, args);
+            Console.WriteLine(lineToWrite);
+#endif
+        }
+
+        static public void DebugWrite(string text, params object[] args)
+        {
+#if DEBUG || OVERRIDE
+            string lineToWrite = string.Format(text, args);
+            Console.Write(lineToWrite);
+#endif
+        }
+
+        static public List<string> GetInput()
+        {
+            var inputFile = File.ReadAllLines("../../input.txt");
+            return new List<string>(inputFile);
+        }
+    }
+}
