fe51291642bf9cf8cb7e999111d4e920ca8a02a8
Richard Carter <Richard-CRT@users.noreply.github.com>
2019-12-09 23:31:55 +0000

    Completed Day 7 & 8

---
+++ 7/IntCodeComputer.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntCodeComputerNS
+{
+    public delegate int InputDelegate();
+    public delegate void OutputDelegate(int output);
+
+    class IntCodeComputer
+    {
+        public enum Opcode { Add, Multiply, Halt };
+        public enum ParameterMode { Position, Immediate };
+        public class Parameter
+        {
+            public ParameterMode Mode;
+            public int Value;
+
+            public int EvaluateParameter(int[] memory)
+            {
+                int evaluation = 0;
+
+                switch (this.Mode)
+                {
+                    case ParameterMode.Immediate:
+                        evaluation = this.Value;
+                        break;
+                    case ParameterMode.Position:
+                        evaluation = memory[this.Value];
+                        break;
+                }
+
+                return evaluation;
+            }
+        }
+
+        public abstract class Instruction
+        {
+            public int Length;
+            public List<Parameter> Parameters = new List<Parameter>();
+
+            public abstract bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, int[] memory);
+        }
+
+        public class AddInstruction : Instruction
+        {
+            public AddInstruction() : base()
+            {
+                this.Length = 4;
+            }
+
+            public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, int[] memory)
+            {
+                if (this.Parameters[2].Mode == ParameterMode.Immediate)
+                    throw new ArgumentException("Parameters that an instruction writes to will never be in immediate mode.");
+                int a = this.Parameters[0].EvaluateParameter(memory);
+                int b = this.Parameters[1].EvaluateParameter(memory);
+                int dest = this.Parameters[2].Value;
+                memory[dest] = a + b;
+                return true;
+            }
+        }
+
+        public class MultiplyInstruction : Instruction
+        {
+            public MultiplyInstruction() : base()
+            {
+                this.Length = 4;
+            }
+
+            public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, int[] memory)
+            {
+                if (this.Parameters[2].Mode == ParameterMode.Immediate)
+                    throw new ArgumentException("Parameters that an instruction writes to will never be in immediate mode.");
+                int a = this.Parameters[0].EvaluateParameter(memory);
+                int b = this.Parameters[1].EvaluateParameter(memory);
+                int dest = this.Parameters[2].Value;
+                memory[dest] = a * b;
+                return true;
+            }
+        }
+
+        public class InputInstruction : Instruction
+        {
+            private InputDelegate InputCallback;
+
+            public InputInstruction(InputDelegate inputCallback) : base()
+            {
+                this.Length = 2;
+                this.InputCallback = inputCallback;
+            }
+
+            public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, int[] memory)
+            {
+                if (this.Parameters[0].Mode == ParameterMode.Immediate)
+                    throw new ArgumentException("Parameters that an instruction writes to will never be in immediate mode.");
+                int input = InputCallback();
+                int dest = this.Parameters[0].Value;
+                memory[dest] = input;
+                return true;
+            }
+        }
+
+        public class OutputInstruction : Instruction
+        {
+            private OutputDelegate OutputCallback;
+
+            public OutputInstruction(OutputDelegate outputCallback) : base()
+            {
+                this.Length = 2;
+                this.OutputCallback = outputCallback;
+            }
+
+            public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, int[] memory)
+            {
+                int a = this.Parameters[0].Value;
+                OutputCallback(memory[a]);
+                return true;
+            }
+        }
+
+        public class JumpIfTrueInstruction : Instruction
+        {
+            public JumpIfTrueInstruction() : base()
+            {
+                this.Length = 3;
+            }
+
+            public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, int[] memory)
+            {
+                int a = this.Parameters[0].EvaluateParameter(memory);
+                if (a != 0)
+                {
+                    int dest = this.Parameters[1].EvaluateParameter(memory);
+                    instructionPointer = dest;
+                    instructionPointerChanged = true;
+                }
+                return true;
+            }
+        }
+
+        public class JumpIfFalseInstruction : Instruction
+        {
+            public JumpIfFalseInstruction() : base()
+            {
+                this.Length = 3;
+            }
+
+            public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, int[] memory)
+            {
+                int a = this.Parameters[0].EvaluateParameter(memory);
+                if (a == 0)
+                {
+                    int dest = this.Parameters[1].EvaluateParameter(memory);
+                    instructionPointer = dest;
+                    instructionPointerChanged = true;
+                }
+                return true;
+            }
+        }
+
+        public class LessThanInstruction : Instruction
+        {
+            public LessThanInstruction() : base()
+            {
+                this.Length = 4;
+                this.Parameters = new List<Parameter>();
+            }
+
+            public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, int[] memory)
+            {
+                if (this.Parameters[2].Mode == ParameterMode.Immediate)
+                    throw new ArgumentException("Parameters that an instruction writes to will never be in immediate mode.");
+                int a = this.Parameters[0].EvaluateParameter(memory);
+                int b = this.Parameters[1].EvaluateParameter(memory);
+                int dest = this.Parameters[2].Value;
+                if (a < b)
+                    memory[dest] = 1;
+                else
+                    memory[dest] = 0;
+                return true;
+            }
+        }
+
+        public class EqualsInstruction : Instruction
+        {
+            public EqualsInstruction() : base()
+            {
+                this.Length = 4;
+            }
+
+            public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, int[] memory)
+            {
+                if (this.Parameters[2].Mode == ParameterMode.Immediate)
+                    throw new ArgumentException("Parameters that an instruction writes to will never be in immediate mode.");
+                int a = this.Parameters[0].EvaluateParameter(memory);
+                int b = this.Parameters[1].EvaluateParameter(memory);
+                int dest = this.Parameters[2].Value;
+                if (a == b)
+                    memory[dest] = 1;
+                else
+                    memory[dest] = 0;
+                return true;
+            }
+        }
+
+        public class HaltInstruction : Instruction
+        {
+            public HaltInstruction() : base()
+            {
+                this.Length = 1;
+            }
+
+            public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, int[] memory)
+            {
+                return false;
+            }
+        }
+
+        private int[] Memory;
+        private InputDelegate InputCallback;
+        private OutputDelegate OutputCallback;
+        private int InstructionPointer = 0;
+
+        public IntCodeComputer(InputDelegate inputCallback, OutputDelegate outputCallback)
+        {
+            this.Memory = new int[] { 99 };
+            this.InputCallback = inputCallback;
+            this.OutputCallback = outputCallback;
+        }
+
+        public void Flash(int[] memory)
+        {
+            this.InstructionPointer = 0;
+            this.Memory = new int[memory.Length];
+            Array.Copy(memory, 0, this.Memory, 0, memory.Length);
+        }
+
+        public Instruction FetchInstruction()
+        {
+            Instruction instruction;
+
+            string complexOpcodeString = Memory[InstructionPointer].ToString().PadLeft(2, '0');
+            string opcodeString = complexOpcodeString.Substring(complexOpcodeString.Length - 2, 2);
+
+            switch (opcodeString)
+            {
+                case "01":
+                    instruction = new AddInstruction();
+                    break;
+                case "02":
+                    instruction = new MultiplyInstruction();
+                    break;
+                case "03":
+                    instruction = new InputInstruction(InputCallback);
+                    break;
+                case "04":
+                    instruction = new OutputInstruction(OutputCallback);
+                    break;
+                case "05":
+                    instruction = new JumpIfTrueInstruction();
+                    break;
+                case "06":
+                    instruction = new JumpIfFalseInstruction();
+                    break;
+                case "07":
+                    instruction = new LessThanInstruction();
+                    break;
+                case "08":
+                    instruction = new EqualsInstruction();
+                    break;
+                case "99":
+                    instruction = new HaltInstruction();
+                    break;
+                default:
+                    throw new ArgumentException("Unknown opcode");
+            }
+
+            complexOpcodeString = complexOpcodeString.PadLeft(instruction.Length + 1, '0');
+
+            for (int i = 1; i < instruction.Length; i++)
+            {
+                Parameter parameter = new Parameter();
+                parameter.Value = Memory[InstructionPointer + i];
+                switch (complexOpcodeString[instruction.Length - (1 + i)])
+                {
+                    case '0':
+                        parameter.Mode = ParameterMode.Position;
+                        break;
+                    case '1':
+                        parameter.Mode = ParameterMode.Immediate;
+                        break;
+                }
+                instruction.Parameters.Add(parameter);
+            }
+
+            return instruction;
+        }
+
+        public bool ExecuteInstruction(Instruction instruction)
+        {
+            // execute instruction
+            bool instructionPointerChanged = false;
+            if (!instructionPointerChanged)
+            {
+                InstructionPointer += instruction.Length;
+            }
+
+            return instruction.Execute(ref InstructionPointer, ref instructionPointerChanged, Memory);
+        }
+
+        public void Run()
+        {
+            for (InstructionPointer = 0; InstructionPointer < Memory.Length;)
+            {
+                Instruction instruction = FetchInstruction();
+                if (!ExecuteInstruction(instruction))
+                {
+                    break;
+                }
+            }
+        }
+
+        public bool RunUntilInput()
+        {
+            Instruction instruction = FetchInstruction();
+            bool firstInstruction = true;
+            while (firstInstruction || !(instruction is InputInstruction))
+            {
+                firstInstruction = false;
+
+                if (!ExecuteInstruction(instruction))
+                {
+                    return false;
+                }
+                instruction = FetchInstruction();
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 8/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCodeUtilities;
+
+namespace _8
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int PictureWidth = 25;
+            const int PictureHeight = 6;
+            const int DigitsInLayer = PictureWidth * PictureHeight;
+
+            List<string> inputList = AoCUtilities.GetInput();
+            string pictureData = inputList[0];
+
+            int layerCount = pictureData.Length / DigitsInLayer;
+
+            List<List<int>> layers = new List<List<int>>();
+
+            int fewestZeros = Int32.MaxValue;
+            int layerNumWithFewestZeros = 0;
+
+            for (int layerNum = 0; layerNum < layerCount; layerNum++)
+            {
+                List<int> layer = new List<int>();
+                int zeroCount = 0;
+                for (int digitNum = 0; digitNum < DigitsInLayer; digitNum++)
+                {
+                    int totalIndex = (layerNum * DigitsInLayer) + digitNum;
+                    int digit = Int32.Parse(pictureData[totalIndex].ToString());
+                    layer.Add(digit);
+
+                    if (digit == 0)
+                        zeroCount++;
+                }
+                layers.Add(layer);
+
+                if (zeroCount < fewestZeros)
+                {
+                    fewestZeros = zeroCount;
+                    layerNumWithFewestZeros = layerNum;
+                }
+            }
+
+            AoCUtilities.DebugWriteLine("Layer with fewest zeros is: {0}", layerNumWithFewestZeros);
+
+            int oneCount = 0;
+            int twoCount = 0;
+            for (int i = 0; i < DigitsInLayer; i++)
+            {
+                int digit = layers[layerNumWithFewestZeros][i];
+
+                if (digit == 1)
+                    oneCount++;
+                else if (digit == 2)
+                    twoCount++;
+            }
+
+            int onesByTwos = oneCount * twoCount;
+            Console.WriteLine("Number of Ones multiplied by number of Twos on layer with fewest Zeros is: {0}", onesByTwos);
+            Console.ReadLine();
+
+            List<int> finalImage = new List<int>();
+            for (int pixelNum = 0; pixelNum < DigitsInLayer; pixelNum ++)
+            {
+                int currentPixel = 2;
+                for (int layerNum = 0; layerNum < layerCount; layerNum ++)
+                {
+                    switch (layers[layerNum][pixelNum])
+                    {
+                        case 2:
+                            continue;
+                        case 1:
+                        case 0:
+                            currentPixel = layers[layerNum][pixelNum];
+                            break;
+                    }
+                    if (currentPixel != 2)
+                    {
+                        break;
+                    }
+                }
+                finalImage.Add(currentPixel);
+            }
+
+            Console.Clear();
+
+            for (int y = 0; y < PictureHeight; y++)
+            {
+                for (int x = 0; x < PictureWidth; x++)
+                {
+                    switch (finalImage[(y*PictureWidth)+x])
+                    {
+                        case 1:
+                            Console.Write("█");
+                            break;
+                        case 0:
+                            Console.Write(" ");
+                            break;
+                        default:
+                            throw new ArgumentException();
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
