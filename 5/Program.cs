2eafa4044180765362f9384e3d3bd3e35de5496a
Richard Carter <Richard-CRT@users.noreply.github.com>
2019-12-09 18:23:33 +0000

    Day 5 complete

--- 5/Program.cs
+++ 5/Program.cs
@@ -13,34 +13,200 @@ namespace _5
     {
         public ParameterMode Mode;
         public int Value;
+
+        public int EvaluateParameter(string[] memory)
+        {
+            int evaluation = 0;
+
+            switch (this.Mode)
+            {
+                case ParameterMode.Immediate:
+                    evaluation = this.Value;
+                    break;
+                case ParameterMode.Position:
+                    evaluation = Int32.Parse(memory[this.Value]);
+                    break;
+            }
+
+            return evaluation;
+        }
     }
 
     public abstract class Instruction
     {
         public int Length;
-        public Opcode Opcode;
-        public List<Parameter> Parameters;
+        public List<Parameter> Parameters = new List<Parameter>();
 
-        public Instruction(Opcode _opcode, List<Parameter> _parameters)
-        {
-            Opcode = _opcode;
-            Parameters = _parameters;
-        }
-
-        public abstract void Execute(int[] memory);
+        public abstract bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, string[] memory);
     }
 
     public class AddInstruction : Instruction
     {
+        public AddInstruction() : base()
+        {
+            this.Length = 4;
+        }
 
-        public AddInstruction(Opcode _opcode, List<Parameter> _parameters) : base(_opcode, _parameters)
+        public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, string[] memory)
+        {
+            if (this.Parameters[2].Mode == ParameterMode.Immediate)
+                throw new ArgumentException("Parameters that an instruction writes to will never be in immediate mode.");
+            int a = this.Parameters[0].EvaluateParameter(memory);
+            int b = this.Parameters[1].EvaluateParameter(memory);
+            int dest = this.Parameters[2].Value;
+            memory[dest] = (a + b).ToString();
+            return true;
+        }
+    }
+
+    public class MultiplyInstruction : Instruction
+    {
+        public MultiplyInstruction() : base()
+        {
+            this.Length = 4;
+        }
+
+        public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, string[] memory)
+        {
+            if (this.Parameters[2].Mode == ParameterMode.Immediate)
+                throw new ArgumentException("Parameters that an instruction writes to will never be in immediate mode.");
+            int a = this.Parameters[0].EvaluateParameter(memory);
+            int b = this.Parameters[1].EvaluateParameter(memory);
+            int dest = this.Parameters[2].Value;
+            memory[dest] = (a * b).ToString();
+            return true;
+        }
+    }
+
+    public class InputInstruction : Instruction
+    {
+        public InputInstruction() : base()
+        {
+            this.Length = 2;
+        }
+
+        public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, string[] memory)
+        {
+            if (this.Parameters[0].Mode == ParameterMode.Immediate)
+                throw new ArgumentException("Parameters that an instruction writes to will never be in immediate mode.");
+            Console.Write("Input: ");
+            string input = Console.ReadLine();
+            int dest = this.Parameters[0].Value;
+            memory[dest] = input;
+            return true;
+        }
+    }
+
+    public class OutputInstruction : Instruction
+    {
+        public OutputInstruction() : base()
+        {
+            this.Length = 2;
+        }
+
+        public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, string[] memory)
+        {
+            int a = this.Parameters[0].Value;
+            Console.WriteLine("Output: {0}", memory[a]);
+            return true;
+        }
+    }
+
+    public class JumpIfTrueInstruction : Instruction
+    {
+        public JumpIfTrueInstruction() : base()
+        {
+            this.Length = 3;
+        }
+
+        public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, string[] memory)
+        {
+            int a = this.Parameters[0].EvaluateParameter(memory);
+            if (a != 0)
+            {
+                int dest = this.Parameters[1].EvaluateParameter(memory);
+                instructionPointer = dest;
+                instructionPointerChanged = true;
+            }
+            return true;
+        }
+    }
+
+    public class JumpIfFalseInstruction : Instruction
+    {
+        public JumpIfFalseInstruction() : base()
+        {
+            this.Length = 3;
+        }
+
+        public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, string[] memory)
+        {
+            int a = this.Parameters[0].EvaluateParameter(memory);
+            if (a == 0)
+            {
+                int dest = this.Parameters[1].EvaluateParameter(memory);
+                instructionPointer = dest;
+                instructionPointerChanged = true;
+            }
+            return true;
+        }
+    }
+
+    public class LessThanInstruction : Instruction
+    {
+        public LessThanInstruction() : base()
         {
             this.Length = 4;
             this.Parameters = new List<Parameter>();
         }
 
-        public override void Execute(int[] memory)
+        public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, string[] memory)
         {
+            if (this.Parameters[2].Mode == ParameterMode.Immediate)
+                throw new ArgumentException("Parameters that an instruction writes to will never be in immediate mode.");
+            int a = this.Parameters[0].EvaluateParameter(memory);
+            int b = this.Parameters[1].EvaluateParameter(memory);
+            int dest = this.Parameters[2].Value;
+            if (a < b)
+                memory[dest] = "1";
+            else
+                memory[dest] = "0";
+            return true;
+        }
+    }
+
+    public class EqualsInstruction : Instruction
+    {
+        public EqualsInstruction() : base()
+        {
+            this.Length = 4;
+        }
+
+        public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, string[] memory)
+        {
+            if (this.Parameters[2].Mode == ParameterMode.Immediate)
+                throw new ArgumentException("Parameters that an instruction writes to will never be in immediate mode.");
+            int a = this.Parameters[0].EvaluateParameter(memory);
+            int b = this.Parameters[1].EvaluateParameter(memory);
+            int dest = this.Parameters[2].Value;
+            if (a == b)
+                memory[dest] = "1";
+            else
+                memory[dest] = "0";
+            return true;
+        }
+    }
+
+    public class HaltInstruction : Instruction
+    {
+        public HaltInstruction() : base()
+        {
+            this.Length = 1;
+        }
+
+        public override bool Execute(ref int instructionPointer, ref bool instructionPointerChanged, string[] memory)
+        {
+            return false;
         }
     }
 
@@ -51,52 +217,81 @@ namespace _5
         {
             List<string> inputList = AoCUtilities.GetInput();
             string program = inputList[0];
-            string[] dataString = program.Split(',');
-            int[] data = new int[dataString.Length];
-            for (int i = 0; i < dataString.Length; i++)
-            {
-                data[i] = Int32.Parse(dataString[i]);
-            }
+            string[] memory = program.Split(',');
 
-            int opcode = -1;
-            int instructionPointer = 0;
-            while (opcode != 99)
+            for (int instructionPointer = 0; instructionPointer < memory.Length;)
             {
-                opcode = data[instructionPointer];
-                int input1Pos;
-                int input2Pos;
-                int outputPos;
-                switch (opcode)
+                Instruction instruction;
+
+                string complexOpcodeString = memory[instructionPointer].ToString().PadLeft(2, '0');
+                string opcodeString = complexOpcodeString.Substring(complexOpcodeString.Length - 2, 2);
+
+                switch (opcodeString)
                 {
-                    case 1: // Add
-                        {
-                            input1Pos = data[instructionPointer + 1];
-                            input2Pos = data[instructionPointer + 2];
-                            outputPos = data[instructionPointer + 3];
-
-                            data[outputPos] = data[input1Pos] + data[input2Pos];
-
-                            instructionPointer += 4;
-                        }
+                    case "01":
+                        instruction = new AddInstruction();
                         break;
-                    case 2: // Multiply
-                        {
-                            input1Pos = data[instructionPointer + 1];
-                            input2Pos = data[instructionPointer + 2];
-                            outputPos = data[instructionPointer + 3];
-
-                            data[outputPos] = data[input1Pos] * data[input2Pos];
-
-                            instructionPointer += 4;
-                        }
+                    case "02":
+                        instruction = new MultiplyInstruction();
                         break;
-                    case 99:
-                        {
-                            instructionPointer += 1;
-                        }
+                    case "03":
+                        instruction = new InputInstruction();
                         break;
+                    case "04":
+                        instruction = new OutputInstruction();
+                        break;
+                    case "05":
+                        instruction = new JumpIfTrueInstruction();
+                        break;
+                    case "06":
+                        instruction = new JumpIfFalseInstruction();
+                        break;
+                    case "07":
+                        instruction = new LessThanInstruction();
+                        break;
+                    case "08":
+                        instruction = new EqualsInstruction();
+                        break;
+                    case "99":
+                        instruction = new HaltInstruction();
+                        break;
+                    default:
+                        throw new ArgumentException("Unknown opcode");
+                }
+
+                complexOpcodeString = complexOpcodeString.PadLeft(instruction.Length + 1, '0');
+
+                for (int i = 1; i < instruction.Length; i++)
+                {
+                    Parameter parameter = new Parameter();
+                    parameter.Value = Int32.Parse(memory[instructionPointer + i]);
+                    switch (complexOpcodeString[instruction.Length - (1 + i)])
+                    {
+                        case '0':
+                            parameter.Mode = ParameterMode.Position;
+                            break;
+                        case '1':
+                            parameter.Mode = ParameterMode.Immediate;
+                            break;
+                    }
+                    instruction.Parameters.Add(parameter);
+                }
+
+                // execute instruction
+                bool instructionPointerChanged = false;
+                if (!instruction.Execute(ref instructionPointer, ref instructionPointerChanged, memory))
+                {
+                    break;
+                }
+
+                if (!instructionPointerChanged)
+                {
+                    instructionPointer += instruction.Length;
                 }
             }
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
         }
     }
 }
