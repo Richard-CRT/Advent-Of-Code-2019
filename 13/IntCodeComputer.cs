06c25b9434d06a6381a18805ed04f2978331f8fb
Richard Carter <Richard-CRT@users.noreply.github.com>
2019-12-13 16:14:03 +0000

    Day 13 Complete - That was fun!

---
+++ 13/IntCodeComputer.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntCodeComputerNS
+{
+    public delegate long InputDelegate();
+    public delegate void OutputDelegate(long output);
+
+    class IntCodeComputer
+    {
+        public enum Opcode { Add, Multiply, Halt };
+        public enum ParameterMode { Position, Relative, Immediate };
+        public class Parameter
+        {
+            private ParameterMode Mode;
+            private long Value;
+
+            public Parameter(ParameterMode parameterMode, long value)
+            {
+                this.Mode = parameterMode;
+                this.Value = value;
+            }
+
+            public long EvaluateParameter(State currentState)
+            {
+                long evaluation = 0;
+
+                switch (this.Mode)
+                {
+                    case ParameterMode.Immediate:
+                        evaluation = this.Value;
+                        break;
+                    case ParameterMode.Position:
+                        evaluation = currentState.Memory[this.Value];
+                        break;
+                    case ParameterMode.Relative:
+                        evaluation = currentState.Memory[this.Value + currentState.RelativeBase];
+                        break;
+                }
+
+                return evaluation;
+            }
+
+            public void WriteParameter(State currentState, long data)
+            {
+                switch (this.Mode)
+                {
+                    case ParameterMode.Position:
+                        currentState.Memory[this.Value] = data;
+                        break;
+                    case ParameterMode.Relative:
+                        currentState.Memory[this.Value + currentState.RelativeBase] = data;
+                        break;
+                    case ParameterMode.Immediate:
+                        throw new ArgumentException("'Parameters that an instruction writes to will never be in immediate mode.'");
+                }
+            }
+        }
+
+        public abstract class Instruction
+        {
+            public int Length;
+            public List<Parameter> Parameters = new List<Parameter>();
+
+            public abstract bool Execute(ref bool instructionPointerChanged, State currentState);
+        }
+
+        public class AddInstruction : Instruction
+        {
+            public AddInstruction() : base()
+            {
+                this.Length = 4;
+            }
+
+            public override bool Execute(ref bool instructionPointerChanged, State currentState)
+            {
+                long a = this.Parameters[0].EvaluateParameter(currentState);
+                long b = this.Parameters[1].EvaluateParameter(currentState);
+                this.Parameters[2].WriteParameter(currentState, a + b);
+                return true;
+            }
+        }
+
+        public class MultiplyInstruction : Instruction
+        {
+            public MultiplyInstruction() : base()
+            {
+                this.Length = 4;
+            }
+
+            public override bool Execute(ref bool instructionPointerChanged, State currentState)
+            {
+                long a = this.Parameters[0].EvaluateParameter(currentState);
+                long b = this.Parameters[1].EvaluateParameter(currentState);
+                this.Parameters[2].WriteParameter(currentState, a * b);
+                return true;
+            }
+        }
+
+        public class InputInstruction : Instruction
+        {
+            private InputDelegate InputCallback;
+
+            public InputInstruction(InputDelegate inputCallback) : base()
+            {
+                this.Length = 2;
+                this.InputCallback = inputCallback;
+            }
+
+            public override bool Execute(ref bool instructionPointerChanged, State currentState)
+            {
+                long input = InputCallback();
+                this.Parameters[0].WriteParameter(currentState, input);
+                return true;
+            }
+        }
+
+        public class OutputInstruction : Instruction
+        {
+            private OutputDelegate OutputCallback;
+
+            public OutputInstruction(OutputDelegate outputCallback) : base()
+            {
+                this.Length = 2;
+                this.OutputCallback = outputCallback;
+            }
+
+            public override bool Execute(ref bool instructionPointerChanged, State currentState)
+            {
+                long a = this.Parameters[0].EvaluateParameter(currentState);
+                OutputCallback(a);
+                return true;
+            }
+        }
+
+        public class JumpIfTrueInstruction : Instruction
+        {
+            public JumpIfTrueInstruction() : base()
+            {
+                this.Length = 3;
+            }
+
+            public override bool Execute(ref bool instructionPointerChanged, State currentState)
+            {
+                long a = this.Parameters[0].EvaluateParameter(currentState);
+                if (a != 0)
+                {
+                    long dest = this.Parameters[1].EvaluateParameter(currentState);
+                    currentState.InstructionPointer = dest;
+                    instructionPointerChanged = true;
+                }
+                return true;
+            }
+        }
+
+        public class JumpIfFalseInstruction : Instruction
+        {
+            public JumpIfFalseInstruction() : base()
+            {
+                this.Length = 3;
+            }
+
+            public override bool Execute(ref bool instructionPointerChanged, State currentState)
+            {
+                long a = this.Parameters[0].EvaluateParameter(currentState);
+                if (a == 0)
+                {
+                    long dest = this.Parameters[1].EvaluateParameter(currentState);
+                    currentState.InstructionPointer = dest;
+                    instructionPointerChanged = true;
+                }
+                return true;
+            }
+        }
+
+        public class LessThanInstruction : Instruction
+        {
+            public LessThanInstruction() : base()
+            {
+                this.Length = 4;
+                this.Parameters = new List<Parameter>();
+            }
+
+            public override bool Execute(ref bool instructionPointerChanged, State currentState)
+            {
+                long a = this.Parameters[0].EvaluateParameter(currentState);
+                long b = this.Parameters[1].EvaluateParameter(currentState);
+                if (a < b)
+                    this.Parameters[2].WriteParameter(currentState, 1);
+                else
+                    this.Parameters[2].WriteParameter(currentState, 0);
+                return true;
+            }
+        }
+
+        public class EqualsInstruction : Instruction
+        {
+            public EqualsInstruction() : base()
+            {
+                this.Length = 4;
+            }
+
+            public override bool Execute(ref bool instructionPointerChanged, State currentState)
+            {
+                long a = this.Parameters[0].EvaluateParameter(currentState);
+                long b = this.Parameters[1].EvaluateParameter(currentState);
+                if (a == b)
+                    this.Parameters[2].WriteParameter(currentState, 1);
+                else
+                    this.Parameters[2].WriteParameter(currentState, 0);
+                return true;
+            }
+        }
+        public class AdjustRelativeBaseInstruction : Instruction
+        {
+            public AdjustRelativeBaseInstruction() : base()
+            {
+                this.Length = 2;
+            }
+
+            public override bool Execute(ref bool instructionPointerChanged, State currentState)
+            {
+                long a = this.Parameters[0].EvaluateParameter(currentState);
+                currentState.RelativeBase += a;
+                return true;
+            }
+        }
+
+        public class HaltInstruction : Instruction
+        {
+            public HaltInstruction() : base()
+            {
+                this.Length = 1;
+            }
+
+            public override bool Execute(ref bool instructionPointerChanged, State currentState)
+            {
+                return false;
+            }
+        }
+
+        public class State
+        {
+            public long[] Memory;
+            public long InstructionPointer = 0;
+            public long RelativeBase = 0;
+        }
+
+        private State CurrentState;
+        private InputDelegate InputCallback;
+        private OutputDelegate OutputCallback;
+
+        public IntCodeComputer(InputDelegate inputCallback, OutputDelegate outputCallback)
+        {
+            CurrentState = new State();
+
+            this.CurrentState.Memory = new long[] { 99 };
+            this.InputCallback = inputCallback;
+            this.OutputCallback = outputCallback;
+        }
+
+        public void Reset()
+        {
+            this.CurrentState.InstructionPointer = 0;
+            this.CurrentState.RelativeBase = 0;
+        }
+
+        public void Flash(long[] memory)
+        {
+            this.CurrentState.Memory = new long[Int16.MaxValue];
+            Array.Clear(this.CurrentState.Memory, 0, Int16.MaxValue);
+            Array.Copy(memory, 0, this.CurrentState.Memory, 0, memory.Length);
+            Reset();
+        }
+
+        public Instruction FetchInstruction()
+        {
+            Instruction instruction;
+
+            string complexOpcodeString = CurrentState.Memory[CurrentState.InstructionPointer].ToString().PadLeft(2, '0');
+            int opcode = Int32.Parse(complexOpcodeString.Substring(complexOpcodeString.Length - 2, 2));
+
+            switch (opcode)
+            {
+                case 1:
+                    instruction = new AddInstruction();
+                    break;
+                case 2:
+                    instruction = new MultiplyInstruction();
+                    break;
+                case 3:
+                    instruction = new InputInstruction(InputCallback);
+                    break;
+                case 4:
+                    instruction = new OutputInstruction(OutputCallback);
+                    break;
+                case 5:
+                    instruction = new JumpIfTrueInstruction();
+                    break;
+                case 6:
+                    instruction = new JumpIfFalseInstruction();
+                    break;
+                case 7:
+                    instruction = new LessThanInstruction();
+                    break;
+                case 8:
+                    instruction = new EqualsInstruction();
+                    break;
+                case 9:
+                    instruction = new AdjustRelativeBaseInstruction();
+                    break;
+                case 99:
+                    instruction = new HaltInstruction();
+                    break;
+                default:
+                    throw new ArgumentException("Unknown opcode");
+            }
+
+            complexOpcodeString = complexOpcodeString.PadLeft(instruction.Length + 1, '0');
+
+            for (int i = 1; i < instruction.Length; i++)
+            {
+                long value = CurrentState.Memory[CurrentState.InstructionPointer + i];
+                ParameterMode mode;
+
+                switch (complexOpcodeString[instruction.Length - (1 + i)])
+                {
+                    case '0':
+                        mode = ParameterMode.Position;
+                        break;
+                    case '1':
+                        mode = ParameterMode.Immediate;
+                        break;
+                    case '2':
+                        mode = ParameterMode.Relative;
+                        break;
+                    default:
+                        throw new ArgumentException("None of the parameter modes match");
+                }
+
+                Parameter parameter = new Parameter(mode, value);
+                instruction.Parameters.Add(parameter);
+            }
+
+            return instruction;
+        }
+
+        public bool ExecuteInstruction(Instruction instruction)
+        {
+            // execute instruction
+            bool instructionPointerChanged = false;
+            if (!instructionPointerChanged)
+            {
+                CurrentState.InstructionPointer += instruction.Length;
+            }
+
+            return instruction.Execute(ref instructionPointerChanged, CurrentState);
+        }
+
+        public void Run()
+        {
+            for (CurrentState.InstructionPointer = 0; CurrentState.InstructionPointer < CurrentState.Memory.Length;)
+            {
+                Instruction instruction = FetchInstruction();
+                if (!ExecuteInstruction(instruction))
+                {
+                    break;
+                }
+            }
+        }
+
+        public bool RunUntilInput()
+        {
+            Instruction instruction = FetchInstruction();
+            bool firstInstruction = true;
+            while (firstInstruction || !(instruction is InputInstruction))
+            {
+                firstInstruction = false;
+
+                if (!ExecuteInstruction(instruction))
+                {
+                    return false;
+                }
+                instruction = FetchInstruction();
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 13/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCodeUtilities;
+using IntCodeComputerNS;
+
+namespace _13
+{
+    enum Tile { Empty = 0, Wall = 1, Block = 2, HorizontalPaddle = 3, Ball = 4 };
+
+    class Program
+    {
+        static long InputMethod()
+        {
+            int ballX = -1;
+            int paddleX = -1;
+            for (int y = 0; y < GridHeight; y++)
+            {
+                for (int x = 0; x < GridWidth; x++)
+                {
+                    if (Grid[y][x] == Tile.Ball)
+                    {
+                        ballX = x;
+                    }
+                    else if (Grid[y][x] == Tile.HorizontalPaddle)
+                    {
+                        paddleX = x;
+                    }
+
+                    if (ballX != -1 && paddleX != -1)
+                    {
+                        break;
+                    }
+                }
+
+                if (ballX != -1 && paddleX != -1)
+                {
+                    break;
+                }
+            }
+
+            long input = 0;
+
+            if (ballX > paddleX)
+            {
+                input = 1;
+            }
+            else if (ballX < paddleX)
+            {
+                input = -1;
+            }
+
+            //AoCUtilities.DebugWrite("Input: {0}", input);
+            //long input = Int64.Parse(Console.ReadLine());
+
+            return input;
+        }
+
+        static List<int> Outputs = new List<int>();
+
+        static void OutputMethod(long output)
+        {
+            //AoCUtilities.DebugWriteLine("Output: {0}", output);
+            Outputs.Add((int)output);
+
+            if (Outputs.Count() == 3)
+            {
+                int x = Outputs[0];
+                int y = Outputs[1];
+
+                if (x == -1 && y == 0)
+                {
+                    Score = Outputs[2];
+                }
+                else
+                {
+                    Tile tileType = (Tile)Outputs[2];
+                    Grid[y][x] = tileType;
+                }
+                Outputs.Clear();
+
+                PrintGrid();
+            }
+        }
+
+        const int GridWidth = 37;
+        const int GridHeight = 22;
+        static List<List<Tile>> Grid = new List<List<Tile>>();
+        static int Score = 0;
+
+        static void PrintGrid()
+        {
+
+#if DEBUG
+            string print = "";
+            print += "Score: " + Score + "\n";
+
+            for (int y = 0; y < GridHeight; y++)
+            {
+                Grid.Add(new List<Tile>());
+                for (int x = 0; x < GridWidth; x++)
+                {
+                    switch (Grid[y][x])
+                    {
+                        case Tile.Empty:
+                            print += "  ";
+                            break;
+                        case Tile.Wall:
+                            print += "██";
+                            break;
+                        case Tile.Block:
+                            print += "▒▒";
+                            break;
+                        case Tile.HorizontalPaddle:
+                            print += "▀▀";
+                            break;
+                        case Tile.Ball:
+                            print += "▓▓";
+                            break;
+                    }
+                }
+                print += "\n";
+            }
+            
+            AoCUtilities.DebugClear();
+            AoCUtilities.DebugWriteLine(print);
+#endif
+
+            //AoCUtilities.DebugReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+
+            List<string> inputList = AoCUtilities.GetInput();
+            string program = inputList[0];
+            string[] data = program.Split(',');
+            long[] memory = new long[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                memory[i] = Int64.Parse(data[i]);
+            }
+            memory[0] = 2;
+
+            for (int y = 0; y < GridHeight; y++)
+            {
+                Grid.Add(new List<Tile>());
+                for (int x = 0; x < GridWidth; x++)
+                {
+                    Grid[y].Add(Tile.Empty);
+                }
+            }
+
+            IntCodeComputer computer = new IntCodeComputer(InputMethod, OutputMethod);
+            computer.Flash(memory);
+            computer.Run();
+
+            int howManyBlockTiles = 0;
+            for (int y = 0; y < GridHeight; y++)
+            {
+                Grid.Add(new List<Tile>());
+                for (int x = 0; x < GridWidth; x++)
+                {
+                    if (Grid[y][x] == Tile.Block)
+                    {
+                        howManyBlockTiles++;
+                    }
+                }
+            }
+
+            Console.WriteLine("When the game exits there are {0} block tiles", howManyBlockTiles);
+            Console.WriteLine("When the game exits the score is {0}", Score);
+            Console.ReadLine();
+        }
+    }
+}
